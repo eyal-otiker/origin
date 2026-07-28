3a0eb411c68f38c3e55ffd48c6f8eb3b64b2ff92
eyal <eyal13102@gmail.com>
2026-07-28 18:35:06 +0300

    [ADD] App ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,88 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TrackResult } from './models/track-result.model';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { SearchStateService } from './services/search-state.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('sound-api');
+
+export class App implements OnInit {
+  public searchQuery: string = '';
+  public viewMode: 'list' | 'tile' = 'list'; 
+  public selectedTrack: TrackResult | null = null;
+  public isFlying: boolean = false; 
+  public isPlaying: boolean = false;
+  public safeEmbedUrl: SafeResourceUrl | null = null;
+
+  constructor(
+    public searchState: SearchStateService,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    const savedView = localStorage.getItem('preferred_view_mode') as 'list' | 'tile';
+    if (savedView) {
+      this.viewMode = savedView;
+    }
+  }
+
+  public onSearch(): void {
+    if (this.searchQuery.trim()) {
+      this.searchState.triggerSearch(this.searchQuery);
+    }
+  }
+
+  public onHistorySelect(term: string): void {
+    this.searchQuery = term;
+    this.searchState.triggerSearch(term);
+  }
+
+  public onNextPage(): void {
+    if (this.searchState.nextCursor) {
+      this.searchState.triggerSearch(this.searchState.currentQuery, this.searchState.nextCursor);
+    }
+  }
+
+  public onPrevPage(): void {
+    if (this.searchState.prevCursor) {
+      this.searchState.triggerSearch(this.searchState.currentQuery, this.searchState.prevCursor);
+    }
+  }
+
+  public toggleViewMode(mode: 'list' | 'tile'): void {
+    this.viewMode = mode;
+    localStorage.setItem('preferred_view_mode', mode);
+  }
+
+  public onSelectTrack(track: TrackResult): void {
+    this.isFlying = true;
+    this.isPlaying = false; 
+    this.safeEmbedUrl = null;
+
+    setTimeout(() => {
+      this.selectedTrack = track;
+      this.isFlying = false;
+    }, 400);
+  }
+
+  public onPlayCentralImage(): void {
+    if (this.selectedTrack) {
+      this.safeEmbedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.selectedTrack.audioEmbedUrl);
+      this.isPlaying = true;
+    }
+  }
+
+  public onRetry(): void {
+    if (this.searchState.currentQuery) {
+      this.searchState.triggerSearch(this.searchState.currentQuery);
+    }
+  }
 }
