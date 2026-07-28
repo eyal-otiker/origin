22212b54a73956f71fc441c7fb7cba0546c8e2d0
eyal <eyal13102@gmail.com>
2026-07-28 18:33:52 +0300

    [MINOR] Config Services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { SoundApiService } from './models/sound-api.model';
+import { MixcloudApiService } from './services/mixcloud.api.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: SoundApiService, 
+      useClass: MixcloudApiService
+    }
   ]
 };
